ba7b762fb3e9e18aa138ca77dc6e8a89578de9ad
Erlein Nicole <erleinureta@gmail.com>
2023-07-09 12:42:31 +0800

    program done

--- File Editor/Project Files/src/App.java
+++ File Editor/Project Files/src/App.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -31,12 +32,11 @@ public class App extends Application {
         mainStage = stage;
         
         textArea = new TextArea(); 
-
         //set size
-
+        textArea.setPrefColumnCount(60);
+        textArea.setPrefRowCount(30);
         //create a BorderPane
         BorderPane root = new BorderPane(textArea);
-
         //place items in the root
         root.setTop(createMenuBar());
         stage.setTitle("Quick Edit~~");
@@ -59,12 +59,20 @@ public class App extends Application {
         fMenu.getItems().add(newF);
         //open a file
         MenuItem openF = new MenuItem("Open..");
-        newF.setOnAction(e -> doOpen());
+        openF.setOnAction(e -> doOpen());
         fMenu.getItems().add(openF);
-
-        MenuItem saveF = new MenuItem("Save");
-        newF.setOnAction(e -> doSave());
+        
+        //save the file
+        MenuItem saveF = new MenuItem("Save Changes");
+        saveF.setOnAction(e -> doSave());
         fMenu.getItems().add(saveF);
+        
+        fMenu.getItems().add(new SeparatorMenuItem());
+
+        //quit program
+        MenuItem quitF = new MenuItem("Quit");
+        quitF.setOnAction(e -> doQuit());
+        fMenu.getItems().add(quitF);
 
         return mBar;
     }
@@ -72,6 +80,7 @@ public class App extends Application {
     //set the text area empty
     private void doNew(){
         textArea.setText("");
+        System.out.println("Im working");
         mainFile = null;
         mainStage.setTitle("New, Untitled file");
     }
@@ -81,12 +90,11 @@ public class App extends Application {
         FileChooser fileDialog = new FileChooser();
         if(mainFile == null){ //it's null becuase it's not being edited.
             fileDialog.setInitialFileName("filename.txt");
-            //this will set the directory in the dialog to 
-            // a user's home directory, or the folder where these program files are kept.
+            //open file manager to This PC
             fileDialog.setInitialDirectory(new File(System.getProperty("user.home")));
         }
         else{
-            //currently being edited
+            //currently being edited: the contents are already being shown sa textArea
             //get that file's name
             fileDialog.setInitialFileName(mainFile.getName());
             //get the file's location
@@ -109,20 +117,76 @@ public class App extends Application {
             error.showAndWait();
             return;
         }
-
-
-
-
-
-
+        // copy contents from textArea, close printwriter and save it by setting the mainFile as the selectedFile
+        try{
+            out.print(textArea.getText());
+            out.close();
+            mainFile = selectedFile;
+            mainStage.setTitle("Edit: " + mainFile.getName());
+        } catch(Exception e){
+            Alert error = new Alert(Alert.AlertType.ERROR, "An error was occured while trying to write data.");
+            error.showAndWait();
+            //return;
+        }
     }
 
     private void doOpen(){
+        FileChooser openDialog = new FileChooser();
+        openDialog.setTitle("Select file to be opened");
+        openDialog.setInitialFileName(null); // no file selected yet bruh
+        if(mainFile == null){
+            openDialog.setInitialDirectory(new File(System.getProperty("user.home")));
+        }
+        else{
+            //if the file is currently being edited, set the file name to its original name
+            openDialog.setInitialFileName(mainFile.getName());
+            //get the file's location
+            openDialog.setInitialDirectory(mainFile.getParentFile());
+        }
+        ////to avoid overriding a file, create a temporary file to hold the contents of
+        // a selected file, then set the program mainfile equal to that selectedFile.
+        File selectedFile = openDialog.showOpenDialog(mainStage);
+        Scanner in;
+        try{
+            in = new Scanner(selectedFile);
+        } catch(Exception e){
+            Alert error = new Alert(Alert.AlertType.ERROR, "An error was encountered while trying to save file.");
+            error.showAndWait();
+            return;
+        }
+        try{
+            StringBuilder inp = new StringBuilder();
+            while(in.hasNextLine()){
+                String str = in.nextLine();
+                if(str == null){
+                    break; //EOF
+                }
+                inp.append(str);
+                inp.append("\n");
+                //limit the texts to a maximum of 1000 characters only
 
+                if(str.length() > 1000){
+                    Alert error = new Alert(Alert.AlertType.ERROR, "Error: File too large...");
+                    error.showAndWait();    
+                    return;
+                }
+            }
+            textArea.setText(inp.toString());
+            mainFile = selectedFile; 
+            mainStage.setTitle("Edit: " + mainFile.getName());
+        }catch(Exception e){
+            Alert error = new Alert(Alert.AlertType.ERROR, "An error was encountered while trying to read file data.");
+            error.showAndWait();    
+            return;
+        }
+        finally{
+            in.close();
+        }
     }
 
-    
-
+    private void doQuit(){
+        System.exit(0);
+    }
 
 }
 
